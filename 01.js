98227a00b55585aba0a7582931861818c5676bca
Komandr33 <and.kom2017@gmail.com>
2023-02-28 23:09:50 +0300

    fitst commit

---
+++ 01.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+function barTriang(p1, p2, p3) {
+    let a = (p1[0] + p2[0] + p3[0]) / 3;
+    let b = (p1[1] + p2[1] + p3[1]) / 3;
+    let c = [+a.toFixed(4), +b.toFixed(4)];
+    return c;
+}
+
+
+
+
+
