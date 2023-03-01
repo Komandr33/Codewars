feda6126e37de35e0a715bef925c5e227de10ca2
AndreyKomarov <and.kom2017@gmail.com>
2023-03-01 20:51:58 +0300

    Update 00.js

--- 00.js
+++ 00.js
@@ -8,3 +8,70 @@ function countSheeps(arrayOfSheep) {
   }
   return a;
 };
+
+//------------------------------------------------------------------
+
+function barTriang(p1, p2, p3) {
+    let a = (p1[0] + p2[0] + p3[0]) / 3;
+    let b = (p1[1] + p2[1] + p3[1]) / 3;
+    let c = [+a.toFixed(4), +b.toFixed(4)];
+    return c;
+}
+
+//------------------------------------------------------------------
+
+function cubeChecker(volume, side) {
+    let a = (side * side * side);
+    if (a != volume || a <= 0) {
+        return false;
+    } else {
+        return true;
+    }
+};
+
+//------------------------------------------------------------------
+
+function getStatus(isBusy) {
+    var msg = (isBusy ? "busy" : "available");
+    return {
+        status: msg
+    }
+}
+
+//------------------------------------------------------------------
+
+function chromosomeCheck(sperm) {
+    if (sperm == 'XY') {
+        return "Congratulations! You're going to have a son."
+    } else if (sperm == 'XX') {
+        return "Congratulations! You're going to have a daughter."
+    }
+}
+
+//------------------------------------------------------------------
+
+function describeAge(age) {
+    if (age <= 12) {
+        return "You're a(n) kid";
+    } else if (age >= 13 && age <= 17) {
+        return "You're a(n) teenager";
+    } else if (age >= 18 && age <= 64) {
+        return "You're a(n) adult";
+    } else {
+        return "You're a(n) elderly";
+    }
+}
+// abbreviated function
+var describeAge = a => `You're a(n) ${a < 13 ? "kid" : a < 18 ? "teenager" : a < 65 ? "adult" : "elderly"}`;
+
+//-----------------------------------------------------------------
+
+function find(array, element) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] == element) return i;
+    } return "Not found";
+}
+// abbreviated function
+let find = (a, e) => a.includes(e) ? a.indexOf(e) : "Not found";
+
+//-----------------------------------------------------------------
