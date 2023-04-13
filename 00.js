ed9d7167c5e3e7121f579de0d95076d99b33b873
Komandr33 <and.kom2017@gmail.com>
2023-04-14 00:00:39 +0300

    new tasks1

--- 00.js
+++ 00.js
@@ -154,3 +154,21 @@ function DNAtoRNA(dna) {
 }
 
 console.log(DNAtoRNA('GCAT'));
+
+//-----------------------------------------------------------------
+
+const rps = (p1, p2) => {
+    if (p1 !== p2) {
+        if ((p1 == "scissors" && p2 == "paper")
+            || (p1 == "paper" && p2 == "rock")
+            || (p1 == "rock" && p2 == "scissors")) {
+            return "Player 1 won!";
+        } else return "Player 2 won!";
+    } else return "Draw!";
+};
+
+//-----------------------------------------------------------------
+
+function century(year) {
+    return Math.ceil(year / 100);
+}
