6cfbe03bb749dddc40ed91e9159df7b5a784a334
Komandr33 <and.kom2017@gmail.com>
2024-03-30 21:36:41 +0300

    added new tasks

--- 00.js
+++ 00.js
@@ -1068,3 +1068,28 @@ function declareWinner(fighter1, fighter2, firstAttacker) {
 console.log(declareWinner(f1, f2, "Harry"));
 
 //---------------------------------------------------------------------------
+[]-- > "no one likes this"
+["Peter"]-- > "Peter likes this"
+["Jacob", "Alex"]-- > "Jacob and Alex like this"
+["Max", "John", "Mark"]-- > "Max, John and Mark like this"
+["Alex", "Jacob", "Mark", "Max"]-- > "Alex, Jacob and 2 others like this"
+function likes(name) {
+    switch (name.length) {
+        case 0: {
+            return "no one likes this"
+        }
+        case 1: {
+            return `${name[0]} likes this`
+        }
+        case 2: {
+            return `${name[0]} and ${name[1]} likes this`
+        }
+        case 3: {
+            return `${name[0]}, ${name[1]} and ${name[2]} likes this`
+        }
+        default: {
+            return `${name[0]}, ${name[1]} and ${name.length - 2} others likes this`
+        }
+    }
+}
+console.log(likes(["Alex", "Jacob", "Mark",]));
