5cced190f2b25c22862456e18adb3d6a1ad362d5
Komandr33 <and.kom2017@gmail.com>
2024-04-04 22:39:22 +0300

    added new task

--- 00.js
+++ 00.js
@@ -1173,3 +1173,22 @@ function alphabetPosition(text) {
     return result.join(' ');
 }
 console.log(alphabetPosition("The sunset sets at twelve o' clock."));
+
+//---------------------------------------------------------------------------
+function accum(s) {
+    let result = ''
+    for (let i = 0; i < s.length; i++) {
+        result = result + s[i].toUpperCase() + s[i].repeat(i).toLowerCase() + '-'
+    }
+    return result.slice(0, -1)
+}
+console.log(accum('abcRtYd'));
+//---------------------------------------------------------------------------
+
+
+
+//---------------------------------------------------------------------------
+
+
+
+//---------------------------------------------------------------------------
